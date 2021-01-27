fd9c4fffaa4d47809ed11fba38b03b376bd2b808
David Sundqvist <david.sundqvist@elev.ga.ntig.se>
2021-01-27 13:31:29 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Test from './components/Test';
+import Router from './components/Router';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Router />
+        <Test />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/Test.js
@@ -0,0 +1,9 @@
+import './Test.css';
+
+function Test() {
+  const greeting = 'Hello Function Component!';
+ 
+  return <h1>{greeting}</h1>;
+}
+
+export default Test;
